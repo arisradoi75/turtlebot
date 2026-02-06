13682615617627e3fa8ba4dcd27466cfff2aed40
arisradoi <radoiaris14@gmail.com>
2026-02-06 18:06:03 +0200

    first commit

---
+++ src/main/java/com/example/robot/RobotSecurityDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.robot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotSecurityDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RobotSecurityDashboardApplication.class, args);
+    }
+
+}
